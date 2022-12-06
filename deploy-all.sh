80c110c58c5f3fc0f42975cc2ea0b3a58fb14668
Sitong_Lu <493984502@qq.com>
2022-12-05 21:29:08 -0700

    edit the order of port forward command

--- deploy-all.sh
+++ deploy-all.sh
@@ -5,10 +5,12 @@ kubectl apply -f redis/redis-service.yaml
 kubectl apply -f rest/rest-deployment.yaml
 kubectl apply -f rest/rest-service.yaml
 
+kubectl apply -f sql/mysql-secret.yaml
+kubectl apply -f sql/mysql-storage.yaml
+kubectl apply -f sql/mysql-deployment.yaml
+
 kubectl apply -f logs/logs-deployment.yaml
 
 kubectl apply -f worker/worker-deployment.yaml
 
-kubectl apply -f mysql-secret.yaml
-kubectl apply -f mysql-storage.yaml
-kubectl apply -f mysql-deployment.yaml
+
